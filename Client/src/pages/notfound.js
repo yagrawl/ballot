1f06777a2bc9b554b0f7edd57fd4421e677500b2
Yash Agrawal <yagrawl2@gmail.com>
2018-10-12 12:04:48 -0500

    Added not found art & linking

--- Client/src/pages/notfound.js
+++ Client/src/pages/notfound.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import notFoundArt from '../assets/imgs/notfound.png';
+
 class NotFound extends Component {
   render() {
     return (
-      <p>404</p>
+      <div className="App">
+        <header className="App-header">
+          <img src={notFoundArt} className="not-found"/>
+          <p>404 : NOT FOUND</p>
+          <Link to={'./create'}>
+            <button className="ButtonStyle button1">
+              Go Back Home
+            </button>
+          </Link>
+        </header>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const time = require('time');
 
 const port = process.env.PORT || 5000;
 
-// API calls
 app.get('/ip', (req, res) => {
   let ip = req.headers['x-forwarded-for'].toString();
   res.send({ express: ip });
@@ -19,11 +18,8 @@ app.get('/api/list', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-
-  // Serve any static files
   app.use(express.static(path.join(__dirname, 'Client/build')));
 
-  // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'Client/build', 'index.html'));
   });
