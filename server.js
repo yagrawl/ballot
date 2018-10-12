1380c9344ec4d00f404dd85963deb12a961da656
Yash Agrawal <yagrawl2@gmail.com>
2018-10-12 01:26:59 -0500

    added express route connection

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const time = require('time');
 const port = process.env.PORT || 5000;
 
 // API calls
-app.get('/init', (req, res) => {
+app.get('/ip', (req, res) => {
   let ip = req.headers['x-forwarded-for'].toString();
   res.send({ express: ip });
+});
+
+app.get('/api/list', (req, res) => {
+  let list = ["item1", "item2", "item3"];
+  res.json(list);
 });
 
 if (process.env.NODE_ENV === 'production') {
