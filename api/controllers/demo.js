f009c7be60a3534dde9b698c3083c608e74cf668
Yash Agrawal <yagrawl2@gmail.com>
2018-10-21 12:41:15 -0500

    basic local db setup

--- api/controllers/demo.js
+++ api/controllers/demo.js
@@ -1,5 +1,20 @@
+let db = require('../models/connection');
+
 exports.demo_create = (req, res) => {
-  console.log(req.body);
+  let key = req.body.ckey;
+  let value = req.body.cvalue;
+  let con = db.con;
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO `ballot`.`demo` (`key`, `value`) VALUES ('4', 'd')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+
 };
 
 exports.demo_read = (req, res) => {
---
+++ api/models/connection.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+
+exports.con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "ballot"
+});
