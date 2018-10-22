84f42a6d2997c3072552a0e34f0bb710d2f0c303
Yash Agrawal <yagrawl2@gmail.com>
2018-10-21 22:06:47 -0500

    account for local & remote db

--- api/models/connection.js
+++ api/models/connection.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql');
 
+const host = process.env.DB_HOST || "localhost";
+const user = process.env.DB_USERNAME || "root";
+const password = process.env.DB_PASSWORD || "password";
+const database = process.env.DB_NAME || "ballot";
+
 exports.con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: host,
+  user: user,
+  password: password,
+  database: database,
   multipleStatements: true
 });
