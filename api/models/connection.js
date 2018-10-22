057eeb717a0c2b8f143e667036dc6e708a56d4b7
Yash Agrawal <yagrawl2@gmail.com>
2018-10-21 21:55:16 -0500

    changed to jawsdb

--- api/models/connection.js
+++ api/models/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 exports.con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "ballot",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true
 });
