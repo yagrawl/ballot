fb54968d70755cef4411a71356c0b4b632274fb8
Yash Agrawal <yagrawl2@gmail.com>
2018-11-02 09:57:13 -0500

    fixing profile issues in user

--- Client/src/pages/user.js
+++ Client/src/pages/user.js
@@ -61,7 +61,7 @@ class User extends Component {
         <div className="cut-top"></div>
         <div className="active-area">
           <div className="user-details">
-            <center><img className="user-profile-image" src={this.state.response.details.profile_picture}></img></center>
+            <center><img className="user-profile-image" src={`https://graph.facebook.com/v3.2/${this.props.match.params.user_id}/picture?height=400&width=400`}></img></center>
             <p className="user-profile-name">{this.state.response.details.name}</p>
           </div>
           {this.getUserFeed()}
