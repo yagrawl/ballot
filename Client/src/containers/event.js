b6ce9e123b8fa7329342ea64c301745bd8587ddd
Yash Agrawal <yagrawl2@gmail.com>
2018-11-02 14:23:33 -0500

    fix ip

--- Client/src/containers/event.js
+++ Client/src/containers/event.js
@@ -9,28 +9,34 @@ import {
 } from 'react-device-detect';
 
 export const sendEvent = (eventName, eventRoute, eventCreatorId) => {
-  let event = {
-    event_name: eventName,
-    event_time: new Date().getTime(),
-    event_creator_ip: ip.address(),
-    event_creator_id: eventCreatorId,
-    event_route: eventRoute,
-    event_device: {
-      browser: browserName,
-      browser_version: browserVersion,
-      engine: engineName,
-      os: osName,
-      os_version: osVersion
-    }
-  };
+  fetch('/ip')
+  .then(response => response.json())
+  .then(data => {
+    let ip = data.express;
 
-  console.log('%cEvent:', 'background: #222; color: #bada55');
-  console.log('Event Details: ', event);
+    let event = {
+      event_name: eventName,
+      event_time: new Date().getTime(),
+      event_creator_ip: ip,
+      event_creator_id: eventCreatorId,
+      event_route: eventRoute,
+      event_device: {
+        browser: browserName,
+        browser_version: browserVersion,
+        engine: engineName,
+        os: osName,
+        os_version: osVersion
+      }
+    };
 
-  fetch("/api/event", {
-    method: "POST",
-    body: JSON.stringify(event),
-    headers: {"Content-Type": "application/json"}
-  })
-  .then(response => console.log('New User Added: ', response));
+    console.log('%cEvent:', 'background: #222; color: #bada55');
+    console.log('Event Details: ', event);
+
+    fetch("/api/event", {
+      method: "POST",
+      body: JSON.stringify(event),
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(response => console.log('New User Added: ', response));
+  });
 }
