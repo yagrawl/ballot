37c51fb1049ec1a7278953abf0011680e897d0dd
Yash Agrawal <yagrawl2@gmail.com>
2018-11-05 15:05:35 -0600

    send events for stats

--- Client/src/pages/stats.js
+++ Client/src/pages/stats.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Logo from '../components/logo'
+import Loader from '../components/loader'
 
 import { sendEvent } from '../containers/event'
 
@@ -14,7 +15,14 @@ class Stats extends Component {
         poll_count: 0,
         ip_count: 0,
         vote_count: 0
-      }
+      },
+      user: {
+        id: "Unknown",
+        name: "Ballot User",
+        profile_picture: "https://i.imgur.com/fMVORsK.png",
+        email: "theballot@gmail.com"
+      },
+      stats_loaded: false
     }
   }
 
@@ -26,7 +34,8 @@ class Stats extends Component {
         this.setState(
           prevState => ({
             ...prevState,
-            response: data.result
+            response: data.result,
+            stats_loaded: true
           })
         );
         console.log('%cStats: ', 'background: #769564; color: white');
@@ -34,34 +43,58 @@ class Stats extends Component {
       });
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+   if(nextProps.user.id !== prevState.user.id) {
+     console.log('Sent Event');
+     sendEvent("Stats Accessed", "/stats", nextProps.user.id);
+     return { UserUpdate: nextProps.user};
+   }
+    else return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.user !== this.props.user){
+      this.setState(
+        prevState => ({
+          ...prevState,
+          user: this.props.user
+        })
+      );
+    }
+  }
+
   render() {
-    return (
-      <div className="stats-header">
-        <Logo link=""/>
-        <div className="stats-active-area">
-          <p className="user-profile-name">Stats</p>
-          <div className="stats-count-box">
-            <div className="stats-count">
-              <p className="stats-count-count">{this.state.response.vote_count}</p>
-              <p className="stats-count-label">Votes</p>
-            </div>
-            <div className="stats-count">
-              <p className="stats-count-count">{this.state.response.poll_count}</p>
-              <p className="stats-count-label">Polls</p>
-            </div>
-            <div className="stats-count">
-              <p className="stats-count-count">{this.state.response.event_count}</p>
-              <p className="stats-count-label">Events</p>
-            </div>
-            <div className="stats-count">
-              <p className="stats-count-count">{this.state.response.ip_count}</p>
-              <p className="stats-count-label">IPs</p>
+    if(this.state.stats_loaded) {
+      return (
+        <div className="stats-header">
+          <Logo link=""/>
+          <div className="stats-active-area">
+            <p className="user-profile-name">Stats</p>
+            <div className="stats-count-box">
+              <div className="stats-count">
+                <p className="stats-count-count">{this.state.response.vote_count}</p>
+                <p className="stats-count-label">Votes</p>
+              </div>
+              <div className="stats-count">
+                <p className="stats-count-count">{this.state.response.poll_count}</p>
+                <p className="stats-count-label">Polls</p>
+              </div>
+              <div className="stats-count">
+                <p className="stats-count-count">{this.state.response.event_count}</p>
+                <p className="stats-count-label">Events</p>
+              </div>
+              <div className="stats-count">
+                <p className="stats-count-count">{this.state.response.ip_count}</p>
+                <p className="stats-count-label">IPs</p>
+              </div>
             </div>
           </div>
-          {/* {this.getFeed()} */}
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return <Loader />
+    }
   }
 }
 
