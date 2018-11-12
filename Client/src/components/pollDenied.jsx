a3cdc85ca35f4117813df6ce87338b26fd1c61d4
Yash Agrawal <yagrawl2@gmail.com>
2018-11-12 14:32:49 -0600

    add graphics to vpn incognito detection message

--- Client/src/components/pollDenied.jsx
+++ Client/src/components/pollDenied.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 
+import incognito from '../assets/imgs/incognito.png';
+import vpn from '../assets/imgs/vpn.png';
+import incognitovpn from '../assets/imgs/incognito&vpn.png';
+
 const PollDenied = (props) => {
   if(props.reason === 'IV') {
     return (
-      <div className="">
-        <p className="">Incognito Mode + VPN</p>
+      <div className="poll-denied-div">
+        <img className="poll-denied-image" src={incognitovpn}></img>
+        <p className="poll-denied-detection">Incognito Mode & VPN Detected</p>
+        <p className="poll-denied-instruction">Please switch back to normal mode on the browser & turn off VPN to access the poll</p>
       </div>
     );
   }
 
   if(props.reason === 'V') {
     return (
-      <div className="">
-        <p className="">VPN</p>
+      <div className="poll-denied-div">
+        <img className="poll-denied-image" src={vpn}></img>
+        <p className="poll-denied-detection">VPN Detected</p>
+        <p className="poll-denied-instruction">Please turn off VPN to access the poll</p>
       </div>
     );
   }
 
   if(props.reason === 'I') {
     return (
-      <div className="">
-        <p className="">Incognito Mode</p>
+      <div className="poll-denied-div">
+        <img className="poll-denied-image" src={incognito}></img>
+        <p className="poll-denied-detection">Incognito Mode Detected</p>
+        <p className="poll-denied-instruction">Please switch back to normal mode on the browser to access the poll</p>
       </div>
     );
   }
