a6a1562e366aca10c6f833827e365549ac8619e0
Yash Agrawal <yagrawl2@gmail.com>
2018-11-29 12:57:41 -0600

    changed height of embed

--- Client/src/components/embedcode.jsx
+++ Client/src/components/embedcode.jsx
@@ -3,7 +3,7 @@ import Highlight from 'react-highlight.js';
 
 const EmbedCode = (props) => {
   let bodytag = '<body>';
-  let code = `<iframe style="overflow:hidden; display:block; margin: auto;" \n    width="320" height="420" align="middle"\n    src="${props.url}" \n    frameborder="0" scrolling="no">\n</iframe>`;
+  let code = `<iframe style="overflow:hidden; display:block; margin: auto;" \n    width="320" height="380" align="middle"\n    src="${props.url}" \n    frameborder="0" scrolling="no">\n</iframe>`;
   return (
     <div className="embed-code-div">
        <p className="embed-code-title">{`Embed Poll in your website. Copy this code within your ${bodytag} tags`}</p>
