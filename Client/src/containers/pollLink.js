fab35df750d09c85afd8300b168f96267d71634d
Yash Agrawal <yagrawl2@gmail.com>
2018-12-17 09:49:00 -0600

    move to ballot.app

--- Client/src/containers/pollLink.js
+++ Client/src/containers/pollLink.js
@@ -40,7 +40,7 @@ class PollLink extends Component {
 
   render() {
     let poll = this.props;
-    let url = `https://theballot.herokuapp.com/poll/${poll.pollid}`;
+    let url = `https://theballot.app/poll/${poll.pollid}`;
     let title = `Vote on this poll!`;
 
     console.log('%cPoll Link:', 'background: #98C379; color: #1B1D23');
